40effd91ea6ed567006bb4424dbeea6c22604b3d
Crainer <camogan3000@hotmail.com>
2019-01-14 13:45:22 -0500

    tEMPLATE TERMINADO

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <div id="app">
-   
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-
 export default {
   name: "App",
-  components: {
-    
-  }
+  components: {}
 };
 </script>
 
